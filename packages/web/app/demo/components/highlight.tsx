7a740ba0b50c460b492c594ce6773cfb56d89a26
Michael Lodzik <michael.lodzik@gmail.com>
2026-04-15 14:51:31 +0200

    web(demo): syntax highlight the static code examples

---
+++ packages/web/app/demo/components/highlight.tsx
@@ -0,0 +1,24 @@
+import { Fragment, type ReactNode } from 'react';
+
+export function highlightCode(code: string): ReactNode[] {
+  const re = /(\/\/[^\n]*)|(#[^\n]*)|('(?:[^'\\]|\\.)*')|(`(?:[^`\\]|\\.)*`)|("(?:[^"\\]|\\.)*")|(true|false|null|undefined)|(\b(?:const|let|var|await|new|return|if|else|function|async|import|from|export|type|interface)\b)|(\b\d+\b)|([\w$.]+)|([^\w\s])|(\s+)/g;
+  const out: ReactNode[] = [];
+  let m;
+  let i = 0;
+  while ((m = re.exec(code)) !== null) {
+    const [, lineComment, hashComment, singleStr, tmpl, doubleStr, bool, keyword, num, ident, punct] = m;
+    let cls = '';
+    if (lineComment || hashComment) cls = 'tk-comment';
+    else if (singleStr || tmpl || doubleStr) cls = 'tk-string';
+    else if (bool) cls = 'tk-bool';
+    else if (keyword) cls = 'tk-keyword';
+    else if (num) cls = 'tk-number';
+    else if (ident) {
+      if (['JSON', 'session', 'response', 'Byoky', 'Anthropic', 'OpenAI', 'fetch', 'Promise', 'console'].includes(ident)) cls = 'tk-builtin';
+      else if (ident.includes('.')) cls = '';
+      else cls = 'tk-ident';
+    } else if (punct) cls = 'tk-punct';
+    out.push(cls ? <span key={i++} className={cls}>{m[0]}</span> : <Fragment key={i++}>{m[0]}</Fragment>);
+  }
+  return out;
+}
