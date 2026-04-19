8b129a274d1d2ca51f103dd962d8395365a1274e
Michael Lodzik <michael.lodzik@gmail.com>
2026-04-19 14:59:29 +0200

    Bump to v0.7.3 (native 1.0.18/22)

--- packages/android/app/build.gradle.kts
+++ packages/android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.byoky.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.0.17"
+        versionCode = 22
+        versionName = "1.0.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
